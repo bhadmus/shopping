ac69f92d9034385b816852f76eb7e7ea5676424e
Ademola Bhadmus <bhadmusademola.1@gmail.com>
2019-09-06 10:51:40 +0100

    optimised the script

--- cypress/integration/testcases/index.spec.js
+++ cypress/integration/testcases/index.spec.js
@@ -8,23 +8,23 @@ context('Create a guest journey on konga', () => {
 
         cy.visit('/')
 
-        //Search for Sneakers
-        cy.get('.f6ed2_25oVd #jsSearchInput').type('Sneakers')
+        //Search for Politics
+        cy.get('.f6ed2_25oVd #jsSearchInput').type('politics')
 
         //Click search icon
         cy.get('.f6ed2_25oVd button').click()
 
         //Selct a price range
-        cy.get('._7d295_SvW4q .e27b2_1kz0Y:nth-of-type(3) li:nth-of-type(4) label').click()
-        
+        cy.get(':nth-child(3) > .ec98e_ZsoHv > .da38b_1ixZ1 > :nth-child(2) > ._41bb1_2mCF_ > label').click()
+
         //Verify that the sort by is there
         cy.get('.d0efc_IQ7Z9 span').contains('Sort By').should('be.visible')
 
         //Verify that Search results is visible
         cy.get('[name="breadcrumbItem"] > a').contains('Search results').should('be.visible')
         
-        //Select a sneaker
-        cy.get('[href="/product/avia-running-shoes-3653998"] > picture > ._206b4_1rr8y').click()
+        //Select a book
+        cy.get('[href="/product/the-nigerian-revolution-and-the-biafran-war-by-alexander-a-madiebo-4441956"] > .af885_1iPzH > h3').click()
 
         //Verify that 'Sold by' is visible
         //cy.get('._835ed_2kqSg > span').contains('Sold by').should('be.visible')
@@ -39,7 +39,7 @@ context('Create a guest journey on konga', () => {
         cy.get('.f082d_safAL > ._2aac2_3bwnD > ._0a08a_3czMG').click()
 
         //Verify that the Konga logo is visible
-        cy.get('.b72ce_21mjc > ._206b4_1rr8y').should('be.visible')
+        cy.get('.b72ce_21mjc > .f5e10_VzEXF').should('be.visible')
 
         //Verify that 'Login' button is visible
         cy.get('._0a08a_3czMG').contains('Login').should('be.visible')
